c821550cc5bb7f70cf53db03fc125bed6fdd5c6e
a.azizov <a.azizov@cognitivepilot.com>
2023-11-22 19:45:25 +0300

    refector

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,45 @@
-import Categories from "./mainComponents/categories/Categories";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link, } from 'react-router-dom';
 import './Main.scss';
 
+
 const Main = () => {
-    return (
-        <Router>
-            <Categories/>
-        </Router>
-    )
+   const [categoriesList, setCategoriesList] = useState([]);
+
+   useEffect(() => {
+      const getAllCategories = async () => await axios
+         .get('https://dummyjson.com/products/categories')
+         .then(({ data }) => {
+            const filtered = data.filter(category => (
+               category === 'smartphones' ||
+               category === 'motorcycle' ||
+               category === 'home-decoration'
+            ))
+            setCategoriesList(["all", ...filtered])
+         })
+         .catch((error) => console.log(error));
+
+      getAllCategories()
+   }, [])
+
+
+   return (
+      <div className="main">
+         {categoriesList && categoriesList.length > 0 ? (
+            categoriesList.map((category, index) => (
+               <li className='categories__element' key={index}>
+                  <Link to={`/${category}`}>{category.toUpperCase()}</Link>
+               </li>
+            ))
+         ) : (
+            <p>Loading categories...</p>
+         )};
+
+      </div>
+
+   )
 }
 
 export default Main;
